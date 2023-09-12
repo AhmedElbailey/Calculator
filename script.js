996b44e94fe3ad5fbc254a3b5939d8764cbc3614
Ahmed Adel <aa5707139@gmail.com>
2023-09-12 14:58:28 +0300

    Uploaded project files

---
+++ script.js
@@ -0,0 +1,20 @@
+const display = document.querySelector(".display");
+const buttons = document.querySelectorAll("button");
+
+let string = "";
+
+buttons.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      string = eval(string);
+    } else if (e.target.innerHTML == "AC") {
+      string = "";
+    } else if (e.target.innerHTML == "DEL") {
+      string = string.substring(0, string.length - 1);
+    } else {
+      string += e.target.innerHTML;
+    }
+
+    display.value = string;
+  });
+});
