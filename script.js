c2c263fec3955d237ffa0973dc7ae7769fc364ab
Ahmed Adel <aa5707139@gmail.com>
2023-09-12 16:23:50 +0300

    Updated code

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ let string = "";
 
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
+    e.preventDefault();
     if (e.target.innerHTML == "=") {
       string = eval(string).toString();
     } else if (e.target.innerHTML == "AC") {
       string = "";
     } else if (e.target.innerHTML == "DEL") {
-      console.log(string, typeof string);
       string = string.substring(0, string.length - 1);
     } else {
       string += e.target.innerHTML;
@@ -18,4 +18,27 @@ buttons.forEach((button) => {
 
     display.value = string;
   });
+});
+
+window.addEventListener("keydown", (e) => {
+  const key = e.key;
+  if (key == "=" || key == "Enter") {
+    string = eval(string).toString();
+  } else if (key == "Delete") {
+    string = "";
+  } else if (key == "Backspace") {
+    string = string.substring(0, string.length - 1);
+  } else if (
+    Number.isInteger(+key) ||
+    key == "+" ||
+    key == "-" ||
+    key == "*" ||
+    key == "/" ||
+    key == "%" ||
+    key == "."
+  ) {
+    string += key;
+  }
+
+  display.value = string;
 });
