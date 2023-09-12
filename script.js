213963dedba4be3eb68378141e4e25d97757cb4f
Ahmed Adel <aa5707139@gmail.com>
2023-09-12 15:08:11 +0300

    modified

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ let string = "";
 buttons.forEach((button) => {
   button.addEventListener("click", (e) => {
     if (e.target.innerHTML == "=") {
-      string = eval(string);
+      string = eval(string).toString();
     } else if (e.target.innerHTML == "AC") {
       string = "";
     } else if (e.target.innerHTML == "DEL") {
+      console.log(string, typeof string);
       string = string.substring(0, string.length - 1);
     } else {
       string += e.target.innerHTML;
